7fbf9dbe0080222179aff77e19fb30ef0dd49705
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-03-28 16:31:26 +0100

    wip update time block function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,28 @@
+const updateTimeBlock = () => {
+  // get current hour
+  const currentHour = moment().format("H");
+
+  const checkTime = () => {
+    const time = $(".row").attr("data-hour");
+
+    console.log(time);
+    // get data attribute of row
+    // if time = current hour - remove past class and add present
+    // if time > current hour - remove past class and add future
+  };
+
+  $(".row").each(checkTime);
+
+  // check time for each block
+};
+
 const onLoad = () => {
   // get current day from moment, format and set text to element
   const currentDay = moment().format("Do MMMM YYYY");
   $("#currentDay").text(currentDay);
+
+  // update time blocks to past, present or future
+  $(updateTimeBlock);
 };
 
 $(document).ready(onLoad);
